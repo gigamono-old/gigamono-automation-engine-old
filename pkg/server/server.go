f46f8d390f2ca7a9cdeb4b144d9b92f126cfbdfa
Stephen <appcypher@outlook.com>
2021-03-14 16:57:22 +0100

    Make minor refactor

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 
 	"github.com/sageflow/sageengine/pkg/engine"
-	"github.com/sageflow/sageflow/pkg/services/proto"
+	"github.com/sageflow/sageflow/pkg/services/proto/generated"
 
 	"github.com/sageflow/sageflow/pkg/inits"
 	"google.golang.org/grpc"
@@ -35,7 +35,7 @@ func NewEngineServer(app inits.App) (EngineServer, error) {
 // Listen starts a new gRPC server that listens on specified port.
 func (server *EngineServer) Listen() error {
 	// Listen on port using TCP.
-	listener, err := net.Listen("tcp", ":"+strconv.Itoa(server.Config.Server.Engine.Port))
+	listener, err := net.Listen("tcp", fmt.Sprint(":", server.Config.Services.Types.Engine.Port))
 	if err != nil {
 		return err
 	}
@@ -43,17 +43,17 @@ func (server *EngineServer) Listen() error {
 	grpcServer := grpc.NewServer() // Create a gRPC server.
 
 	// Register gRPC service.
-	proto.RegisterEngineServiceServer(grpcServer, server)
+	generated.RegisterEngineServiceServer(grpcServer, server)
 	reflection.Register(grpcServer)
 
 	return grpcServer.Serve(listener) // Listen for requests.
 }
 
 // SayHello says Hello
-func (server *EngineServer) SayHello(ctx context.Context, msg *proto.EngineMessage) (*proto.EngineMessage, error) {
+func (server *EngineServer) SayHello(ctx context.Context, msg *generated.Message) (*generated.Message, error) {
 	engineMsg := "Engine replies: " + msg.Content
 	fmt.Println(engineMsg)
-	response := proto.EngineMessage{
+	response := generated.Message{
 		Content: engineMsg,
 	}
 	return &response, nil
